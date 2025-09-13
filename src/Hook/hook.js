c7cdad2a635ddbae18a4f3b7c5c4856b3b89fc00
Arham5503 <arhamali4510@gmail.com>
2025-09-13 20:08:22 +0500

    Intial Commit

---
+++ src/Hook/hook.js
@@ -0,0 +1,22 @@
+async function hook(link) {
+    try {
+        const response= await fetch("https://cors-anywhere.herokuapp.com/https://cleanuri.com/api/v1/shorten",{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            url:link
+        })
+    })
+   
+    const data=await response.json()
+
+    return data
+    }catch (error) {
+  console.error("Error:", error);
+  alert("Something went wrong. Please try again.");
+}
+
+}
+export default hook
